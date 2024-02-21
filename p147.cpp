4e449f48f2145b0af96bc1dd0d08b25e506740a3
Cat Burglar <62001405+F64081169@users.noreply.github.com>
2024-02-21 10:33:50 +0800

    Update p147.cpp

--- p147.cpp
+++ p147.cpp
@@ -4,19 +4,19 @@ using namespace std;
 vector<int> price = {5,10,20,50,100,200,500,1000,2000,5000,10000};
 
 int main(){
-    float n;
+    double n;
     while(cin >> n && n != 0){
         unsigned long long dp[30001] = {0};
         dp[0] = 1;
         n *= 100;
-        int a = n;
+		int amount = (int)round(n);
 
         for(int i = 0;i<price.size();i++){
-            for(int j = price[i];j<=a;j++){
+            for(int j = price[i];j<=amount;j++){
                 dp[j] += dp[j-price[i]];
             }
         }
-        printf("%6.2f%17llu\n",a/100.0,dp[a]);
+        printf("%6.2f%17llu\n",n/100.0,dp[amount]);
         
     }
 }
